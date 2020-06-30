07b9bffdb925d762c34f3ea4ae03e0501db7b609
asyncee <asyncee@users.noreply.github.com>
2020-06-30 22:57:37 +0300

    Fail installation if .dotfiles directory already exists

--- install.sh
+++ install.sh
@@ -1,9 +1,20 @@
-# Work in home directory.
-cd ~
+#!/bin/bash
+
+fail() {
+    echo -e "\033[33;31m$1\033[33;0m"
+}
+
+DOTFILES='~/.dotfiles'
+
+# Check for dotfiles dir.
+if [ -d "$DOTFILES" ]; then
+    fail "ERROR: $DOTFILES directory already exists! Please remove it and try again."
+    exit
+fi
 
 # Clone dotfiles.
-git clone https://github.com/asyncee/dotfiles.git ~/.dotfiles
+git clone https://github.com/asyncee/dotfiles.git $DOTFILES
 
 # Install zsh
-ln -s ~/.dotfiles/oh-my-zh ~/.oh-my-zsh
-ln -s ~/.dotfiles/oh-my-zsh/zshrc ~/.zshrc
+ln -s $DOTFILES/oh-my-zh ~/.oh-my-zsh || fail "Error: ~/.oh-my-zsh already exists!"
+ln -s $DOTFILES/oh-my-zsh/zshrc ~/.zshrc || fail "Error: ~/.zshrc already exists!"
