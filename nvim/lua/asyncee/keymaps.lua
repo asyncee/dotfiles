b91422ad379b26f212e9d578960d3abe079a9ca7
asyncee <asyncee@users.noreply.github.com>
2022-07-08 08:33:00 +0300

    Update keymaps

--- nvim/lua/asyncee/keymaps.lua
+++ nvim/lua/asyncee/keymaps.lua
@@ -2,82 +2,38 @@ local wk = require("which-key")
 wk.setup({})
 
 -- Terminal
--- Exit by hitting control+f
+-- Toggle normal terminal mode by hitting control+f
 vim.keymap.set("t", "<C-f>", [[<C-\><C-n>]], {})
+wk.register({ ["<C-t>"] = { require("FTerm").toggle, "Terminal" } }, { mode = "n" })
+wk.register({ ["<C-t>"] = { require("FTerm").toggle, "Terminal" } }, { mode = "t" })
+
+-- Code / lsp mappings
 wk.register({
-	["<C-t>"] = { require("FTerm").toggle, "Terminal" },
+	["<Leader>dn"] = { "<cmd>Lspsaga diagnostic_jump_next<CR>", "Diagnostics next" },
+	["<Leader>dp"] = { "<cmd>Lspsaga diagnostic_jump_prev<CR>", "Diagnostics prev" },
+	["gD"] = { "<cmd> Telescope lsp_type_definitions<CR>", "Type definition" },
+	["gd"] = { "<cmd> Telescope lsp_definitions<CR>", "Definition" },
+	["gp"] = { "<cmd>Lspsaga preview_definition<CR>", "Preview definition" },
+	["gi"] = { "<cmd> Telescope lsp_implementations<CR>", "Implementation" },
+	["gr"] = { "<cmd> Telescope lsp_references<CR>", "References" },
+	["gc"] = { "<cmd> Telescope lsp_incoming_calls<CR>", "In calls" },
+	["go"] = { "<cmd> Telescope lsp_outgoing_calls<CR>", "Out calls" },
+	["K"] = { "<cmd>Lspsaga hover_doc<CR>", "Hover" },
+	["<C-k>"] = { "<cmd>Lspsaga signature_help<CR>", "Signature info" },
+	["<Leader>ca"] = { vim.lsp.buf.code_action, "Code action" },
+	["<Leader>rca"] = { vim.lsp.buf.range_code_action, "Range code action" },
+	["<Leader>rn"] = { vim.lsp.buf.rename, "Rename" },
+	["<Leader>ss"] = { "<cmd> Telescope lsp_dynamic_workspace_symbols<CR>", "Show symbols" },
+	["<Leader>sd"] = { "<cmd> Telescope treesitter<CR>", "Show document symbols" },
+
+	-- Golang specific
+	["<C-i>"] = { require("telescope").extensions.goimpl.goimpl, "Implement Go interface" },
 }, { mode = "n" })
+
+-- Code / lsp mappings (visual mode)
 wk.register({
-	["<C-t>"] = { require("FTerm").toggle, "Terminal" },
-}, { mode = "t" })
-
---[[
-vim.keymap.set('n', '<space>e', vim.diagnostic.open_float, opts)
-vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
-vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
-vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, opts)
-
--- Use an on_attach function to only map the following keys
--- after the language server attaches to the current buffer
-local on_attach = function(client, bufnr)
-  -- Enable completion triggered by <c-x><c-o>
-  vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
-
-  -- Mappings.
-  -- See `:help vim.lsp.*` for documentation on any of the below functions
-  local bufopts = { noremap=true, silent=true, buffer=bufnr }
-  vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
-  vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
-  vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
-  vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
-  vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
-  vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, bufopts)
-  vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
-  vim.keymap.set('n', '<space>wl', function()
-    print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-  end, bufopts)
-  vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, bufopts)
-  vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, bufopts)
-  vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, bufopts)
-  vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
-  vim.keymap.set('n', '<space>f', vim.lsp.buf.formatting, bufopts)
-end ]]
-
--- Lsp
-wk.register({
-	["<leader>"] = {
-		c = {
-			name = "+lsp",
-			d = { "<cmd> Telescope lsp_definitions<CR>", "Definition" },
-			r = { "<cmd> Telescope lsp_references<CR>", "References" },
-			i = { "<cmd> Telescope lsp_implementations<CR>", "Implementation" },
-			a = { vim.lsp.buf.code_action, "Code action" },
-			x = { vim.lsp.buf.range_code_action, "Range code action" },
-			o = { "<cmd> Telescope lsp_dynamic_workspace_symbols<CR>", "All symbols" },
-			s = { "<cmd> Telescope treesitter<CR>", "Doc symbols" },
-			c = { "<cmd> Telescope lsp_incoming_calls<CR>", "In calls" },
-			b = { "<cmd> Telescope lsp_outgoing_calls<CR>", "Out calls" },
-			t = { "<cmd> Telescope lsp_type_definition<CR>", "Type definition" },
-			f = { require("telescope").extensions.goimpl.goimpl, "Implement interface" },
-		},
-		r = {
-			name = "+refactor",
-			r = { vim.lsp.buf.rename, "Rename" },
-		},
-	},
-})
-
--- LSP Saga
-wk.register({
-	["<leader>a"] = {
-		name = "+lsp saga",
-		h = { "<cmd>Lspsaga hover_doc<CR>", "Hover" },
-		s = { "<cmd>Lspsaga signature_help<CR>", "Signature info" },
-		d = { "<cmd>Lspsaga preview_definition<CR>", "Preview definition" },
-		n = { "<cmd>Lspsaga diagnostic_jump_next<CR>", "Diagnostics next" },
-		p = { "<cmd>Lspsaga diagnostic_jump_prev<CR>", "Diagnostics prev" },
-	},
-})
+	["<Leader>rca"] = { vim.lsp.buf.range_code_action, "Range code action" },
+}, { mode = "v" })
 
 -- Telescope
 wk.register({
