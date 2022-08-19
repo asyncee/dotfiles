7f91f382ee79731f8e8fca9430af2afa0b178c26
asyncee <asyncee@users.noreply.github.com>
2022-08-19 13:26:02 +0300

    Support for MacOS / Debian / Ubuntu systems

--- install.sh
+++ install.sh
@@ -2,36 +2,30 @@
 
 DOTFILES="$HOME/.dotfiles"
 CONFIG="$HOME/.config"
-PYNVIMENVDIR="$HOME/.pyenv"
+SYSTEM=$(uname -s)
 
+BREW_PACKAGES="git git-flow-avh grc tmux fzf ripgrep the_silver_searcher fd      bat delta stylua"
+DEB_PACKAGES=" git git-flow     grc tmux fzf ripgrep silversearcher-ag   fd-find bat delta        unzip gcc make"
+
+# Install packages
+if [ "$SYSTEM" = "Darwin" ]; then
+    brew install $BREW_PACKAGES
+else
+    sudo apt install $DEB_PACKAGES
+    # stylua
+    if ! [ -x "$(command -v stylua)" ]; then
+        echo ">>> Please install stylua"
+        open "https://github.com/JohnnyMorganz/StyLua#installation"
+    fi
+fi
+
+# Link configuration
 mkdir -p $CONFIG
 
-#
-# Git
-#
-brew install git git-flow-avh
 ln -sFfh "$DOTFILES/git" "$CONFIG/git"
-echo ">>> Installed git config"
-
-#
-# IdeaVIM
-#
 ln -sFfh "$DOTFILES/ideavimrc" "$HOME/.ideavimrc"
-echo ">>> Installed .ideavimrc"
-
-#
-# Hammerspoon
-#
 ln -sFfh "$DOTFILES/hammerspoon" "$HOME/.hammerspoon"
-echo ">>> Installed Hammerspoon config"
-
-#
-# Zsh
-#
-
-# Grc
-brew install grc
-echo ">>> Installed grc"
+ln -sFfh "$DOTFILES/nvim" "$CONFIG/nvim"
 
 # Oh-my-zsh
 rm -rf "$HOME/.oh-my-zsh"
@@ -39,23 +33,12 @@ rm "$HOME/.zshrc"
 RUNZSH=no KEEP_ZSHRC=yes sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 rm "$HOME/.zshrc"
 ln -sFfh "$DOTFILES/oh-my-zsh/zshrc" "$HOME/.zshrc"
-echo ">>> Installed zsh config"
 
-#
 # Vim
-#
-
 if ! [ -x "$(command -v nvim)" ]; then
-    echo ">>> Installed neovim at least 0.8.0"
+    echo ">>> Please, install neovim at least 0.8.0 and then run `nvim +PackerInstall`"
     open https://github.com/neovim/neovim/releases/nightly
+else
+    nvim +PackerInstall
+    nvim +Mason
 fi
-
-brew install tmux
-echo ">>> Installed tmux"
-
-brew install fzf ripgrep ag stylua fd bat delta ag gotags golangci-lint
-echo ">>> Installed extra deps"
-
-ln -sFfh "$DOTFILES/nvim" "$CONFIG/nvim"
-nvim +PackerInstall
-echo ">>> Installed nvim config"
