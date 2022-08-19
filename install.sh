a0aa1bca5f0ccd9053ba1dee312f12d9fbbcc571
asyncee <asyncee@users.noreply.github.com>
2022-08-19 21:09:33 +0300

    Add Fedora support

--- install.sh
+++ install.sh
@@ -4,14 +4,26 @@ DOTFILES="$HOME/.dotfiles"
 CONFIG="$HOME/.config"
 SYSTEM=$(uname -s)
 
-BREW_PACKAGES="git git-flow-avh grc tmux fzf ripgrep the_silver_searcher fd      bat delta stylua"
-DEB_PACKAGES=" git git-flow     grc tmux fzf ripgrep silversearcher-ag   fd-find bat delta        unzip gcc make"
+BREW_PACKAGES="git git-flow-avh grc tmux fzf ripgrep the_silver_searcher fd      bat      delta"
+DEB_PACKAGES=" git git-flow     grc tmux fzf ripgrep silversearcher-ag   fd-find bat      delta gcc"
+RPM_PACKAGES="                  grc tmux fzf ripgrep the_silver_searcher fd-find                gcc zsh"
 
 # Install packages
 if [ "$SYSTEM" = "Darwin" ]; then
     brew install $BREW_PACKAGES
 else
-    sudo apt install $DEB_PACKAGES
+    # Linux Mint
+    if [ -x "$(command -v apt)" ]; then
+        sudo apt install $DEB_PACKAGES
+    # Fedora
+    elif [ -x "$(command -v dnf)" ]; then
+        sudo dnf install $RPM_PACKAGES
+        # Neovim
+        sudo dnf copr enable agriffis/neovim-nightly
+        sudo dnf install -y neovim python3-neovim
+        # git-flow
+        wget -q  https://raw.githubusercontent.com/petervanderdoes/gitflow-avh/develop/contrib/gitflow-installer.sh && sudo bash gitflow-installer.sh install stable; sudo rm -rf ./gitflow-installer.sh* ./gitflow
+    fi
 fi
 
 # Link configuration
