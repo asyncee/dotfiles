15a7836e0eb4591217feafb9293b6d62345e9c3a
asyncee <asyncee@users.noreply.github.com>
2022-11-27 09:04:33 +0300

    remove support for mac and fedora

--- install.sh
+++ install.sh
@@ -6,27 +6,9 @@ DOTFILES="$HOME/.dotfiles"
 CONFIG="$HOME/.config"
 SYSTEM=$(uname -s)
 
-BREW_PACKAGES="git git-flow-avh grc tmux fzf ripgrep the_silver_searcher fd      bat      delta"
-DEB_PACKAGES=" git git-flow     grc tmux fzf ripgrep silversearcher-ag   fd-find bat      delta gcc"
-RPM_PACKAGES="                  grc tmux fzf ripgrep the_silver_searcher fd-find                gcc zsh"
+PACKAGES="zsh git git-flow grc tmux fzf ripgrep silversearcher-ag fd-find bat delta gcc"
 
-# Install packages
-if [ "$SYSTEM" = "Darwin" ]; then
-    brew install $BREW_PACKAGES
-else
-    # Linux Mint
-    if [ -x "$(command -v apt)" ]; then
-        sudo apt install $DEB_PACKAGES
-    # Fedora
-    elif [ -x "$(command -v dnf)" ]; then
-        sudo dnf install $RPM_PACKAGES
-        # Neovim
-        sudo dnf copr enable agriffis/neovim-nightly
-        sudo dnf install -y neovim python3-neovim
-        # git-flow
-        wget -q  https://raw.githubusercontent.com/petervanderdoes/gitflow-avh/develop/contrib/gitflow-installer.sh && sudo bash gitflow-installer.sh install stable; sudo rm -rf ./gitflow-installer.sh* ./gitflow
-    fi
-fi
+sudo apt install $PACKAGES
 
 # Link configuration
 mkdir -p $CONFIG
@@ -34,13 +16,11 @@ mkdir -p $CONFIG
 # TODO: use mv
 rm -rf "$CONFIG/git"
 rm -rf "$CONFIG/nvim"
-rm -rf "$HOME/.hammerspoon"
 rm -rf "$HOME/.ideavimrc"
 rm -rf "$HOME/.tmux.conf"
 
 ln -sf "$DOTFILES/git" "$CONFIG/git"
 ln -sf "$DOTFILES/ideavimrc" "$HOME/.ideavimrc"
-ln -sf "$DOTFILES/hammerspoon" "$HOME/.hammerspoon"
 ln -sf "$DOTFILES/nvim" "$CONFIG/nvim"
 ln -sf "$DOTFILES/tmux.conf" "$HOME/.tmux.conf"
 
@@ -51,11 +31,5 @@ RUNZSH=no KEEP_ZSHRC=yes sh -c "$(curl -fsSL https://raw.githubusercontent.com/o
 rm "$HOME/.zshrc"
 ln -sf "$DOTFILES/oh-my-zsh/zshrc" "$HOME/.zshrc"
 
-# Vim
-if ! [ -x "$(command -v nvim)" ]; then
-    echo ">>> Please, install neovim at least 0.8.0 and then run `nvim +PackerInstall`"
-    open https://github.com/neovim/neovim/releases/nightly
-else
-    nvim +PackerInstall
-    nvim +Mason
-fi
+nvim +PackerInstall
+nvim +Mason
