b3e3fe96fa84da0dd77ea6388b19fb1c934e7e60
asyncee <asyncee@users.noreply.github.com>
2025-07-23 11:34:48 +0300

    update configs

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,7 +1,21 @@
-set -gx fish_greeting
-set -gx EDITOR vim
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
 
-fish_vi_key_bindings
+set fish_greeting
+
+set EDITOR nvim
+set VISUAL nvim
+
+set -g fish_key_bindings fish_vi_key_bindings
+
+fish_add_path /opt/nvim/bin
+
+abbr bat batcat
+
+set -x FZF_DEFAULT_COMMAND 'fdfind --type f --strip-cwd-prefix --hidden --follow --exclude .git'
+set -x FZF_CTRL_T_COMMAND "fdfind $dir --type f --strip-cwd-prefix --hidden --follow --exclude .git"
+set -x FZF_ALT_C_COMMAND "fdfind $dir --type d --strip-cwd-prefix --hidden --follow --exclude .git"
 
 fzf --fish | source
 
@@ -9,6 +23,3 @@ fish_add_path /home/asyncee/.zig
 fish_add_path /home/asyncee/development/flutter/bin
 fish_add_path /home/asyncee/Android/Sdk/platform-tools
 
-if status is-interactive
-    # Commands to run in interactive sessions can go here
-end
